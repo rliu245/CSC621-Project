e63c641c2f52791dd7368b716964b3dfab915d2f
Ray <rliu245@gmail.com>
2017-05-16 12:26:57 -0700

    added necessary includes and function declaration for watershed

--- Final.h
+++ Final.h
@@ -14,7 +14,14 @@
 #include "itkImageSeriesWriter.h"
 #include "itkNumericSeriesFileNames.h"
 
+// Include necessary headers for WatershedSegmenation
+#include "itkWatershedImageFilter.h"
+#include "itkScalarToRGBPixelFunctor.h"
+#include "itkUnaryFunctorImageFilter.h"
+#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
+
 void DicomSeriesReadImageWrite2( char * dicom_directory, char * outputfilename );
 void ImageReadImageSeriesWrite( char * input_file, char * output_prefix, char * output_extension );
+void WatershedSegmentation2( char* input_image, char* output_image, char* lower_threshold, char* output_scalelevel );
 
 #endif  

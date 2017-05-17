756aa8beabfe63bf70dc4ce9955d6e69301e3433
Ray <rliu245@gmail.com>
2017-05-16 20:36:14 -0700

    increased interactability via cmd line and fixed input datatypes

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -11,6 +11,10 @@ int main( int argc, char *argv[] ) {
             << "4. Perform Quantization\n"
             << "5. All of the Above" << std::endl;
 
+  std::cout << "Input: ";
+  std::cin >> choice;
+  std::cout << std::endl;
+
   if(choice == 1) {
     std::cout << "Expecting 3 parameters: DicomDirectory outputPrefix outputExtension" << std::endl;
   } else if(choice == 2) {
@@ -21,13 +25,17 @@ int main( int argc, char *argv[] ) {
 
   } else if(choice == 5) {
     std::cout << "Expecting 3 parameters: DicomDirectory outputPrefix outputExtension" << std::endl;
+    std::string DicomDirectory, outputPrefix, outputExtension;
+    std::cout << "Input: ";
+    std::cin >> DicomDirectory >> outputPrefix >> outputExtension;
+    std::cout << std::endl;
 
     char* dimage = "Image3D.mhd";
 
     std::cout << "Converting dicom series to 3D image" << std::endl;
-    DicomSeriesReadImageWrite2(argv[1], dimage);
+    DicomSeriesReadImageWrite2(DicomDirectory.c_str(), dimage);
     std::cout << "Converting 3D image to png series" << std::endl;
-    ImageReadImageSeriesWrite(dimage, argv[2], argv[3]);
+    ImageReadImageSeriesWrite(dimage, outputPrefix.c_str(), outputExtension.c_str());
 
     std::cout << "\nPerforming Watershed Segmentation now." << std::endl << std::endl;
 
@@ -36,13 +44,23 @@ int main( int argc, char *argv[] ) {
     std::cin >> repeat;
     std::cout << std::endl;
 
-    std::string outputImage, lowerThreshold, outputScaleLevel;
+    std::string temp1, temp2, temp3;
 
     // Perform Watershed Segmentation
     for(int i = 0; i < repeat; i++) {
       std::cout << "Expecting 3 parameters: outputImage lowerThreshold outputScaleLevel" << std::endl;
-      std::cin >> outputImage >> lowerThreshold >> outputScaleLevel;
-      WatershedSegmentation2(dimage, outputImage.c_str(), lowerThreshold.c_str(), outputScaleLevel.c_str());
+      std::cin >> temp1 >> temp2 >> temp3;
+      char* outputImage = (char*) malloc(sizeof(char) * temp1.length());
+      strcpy(outputImage, temp1.c_str());
+      char* lowerThreshold = (char*) malloc(sizeof(char) * temp2.length());
+      strcpy(lowerThreshold, temp2.c_str());
+      char* outputScaleLevel = (char*) malloc(sizeof(char) * temp3.length());
+      strcpy(outputScaleLevel, temp3.c_str());
+      WatershedSegmentation2(dimage, outputImage, lowerThreshold, outputScaleLevel);
+      
+      free(outputImage);
+      free(lowerThreshold);
+      free(outputScaleLevel);
     }
     /*
     std::cout << "\nPerforming Registration now." << std::endl << std::endl;

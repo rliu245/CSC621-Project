9a6f87cd8b4686005ecca577be7ba3bd1a273043
Ray <rliu245@gmail.com>
2017-05-17 17:14:03 -0700

    changed DicomSeries and PNGMaker parameter to const char

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -2,8 +2,8 @@
 #ifndef _IMAGEPROCESSING_H
 #define _IMAGEPROCESSING_H
 
-void DicomSeriesReadImageWrite2(const char * dicom_directory, char * outputfilename );
-void ImageReadImageSeriesWrite( char * input_file, const char * output_prefix, const char * output_extension );
+void DicomSeriesReadImageWrite2(const char * dicom_directory, const char * outputfilename );
+void ImageReadImageSeriesWrite(const char * input_file, const char * output_prefix, const char * output_extension );
 void WatershedSegmentation2(char* input_image, char* output_image, char* lower_threshold, char* output_scalelevel );
 void DemonsRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile );
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile);

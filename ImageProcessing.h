2b867321628c9eaa8fe9966bda783c028176b876
Ray <rliu245@gmail.com>
2017-05-17 17:47:02 -0700

    Changed watershedsegmentation to use const char parameters

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -4,7 +4,7 @@
 
 void DicomSeriesReadImageWrite2(const char * dicom_directory, const char * outputfilename );
 void ImageReadImageSeriesWrite(const char * input_file, const char * output_prefix, const char * output_extension );
-void WatershedSegmentation2(char* input_image, char* output_image, char* lower_threshold, char* output_scalelevel );
+void WatershedSegmentation2(const char* input_image, const char* output_image, const char* lower_threshold, const char* output_scalelevel );
 void DemonsRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile );
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile);
 

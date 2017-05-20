55deaba669ad165ea9ce5fa2e54c792fb4a01b3f
Ray <rliu245@gmail.com>
2017-05-19 17:29:09 -0700

    removed DemonsRegistration function template

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -5,7 +5,6 @@
 void DicomSeriesReadImageWrite2(const char * dicom_directory, const char * outputfilename );
 void ImageReadImageSeriesWrite(const char * input_file, const char * output_prefix, const char * output_extension );
 void WatershedSegmentation2(const char* input_image, const char* output_image, const char* lower_threshold, const char* output_scalelevel );
-void DemonsRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile );
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile);
 
 #endif  
